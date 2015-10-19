59f4fea375252a2137c7026d8cfc0aaf3d79012e
witokondoria <witokondoria@gmail.com>
2015-10-19 12:43:34 +0200

    include running status

--- src/main/java/hudson/plugins/jira/Updater.java
+++ src/main/java/hudson/plugins/jira/Updater.java
@@ -189,13 +189,19 @@ class Updater {
     private static String createComment(Run<?, ?> build,
                                         boolean wikiStyle, String jenkinsRootUrl, boolean recordScmChanges, JiraIssue jiraIssue) {
         if (build instanceof WorkflowRun) {
+            String image;
+            if ( "RUNNING".equals(JiraIssueWorkflowUpdater.status)) {
+                image = BallColor.GREY_ANIME.getImage();
+            } else {
+                image = BallColor.valueOf(JiraIssueWorkflowUpdater.status).getImage();
+            }
             return format(
                     wikiStyle ?
                             "%6$s: Integrated in !%1$simages/16x16/%3$s! [%2$s|%4$s]\n%5$s" :
                             "%6$s: Integrated in Jenkins build %2$s (See [%4$s])\n%5$s",
                     jenkinsRootUrl,
                     build,
-                    BallColor.valueOf(JiraIssueWorkflowUpdater.status).getImage(),
+                    image,
                     Util.encode(jenkinsRootUrl + build.getUrl()),
                     getScmComments(wikiStyle, build, recordScmChanges, jiraIssue),
                     JiraIssueWorkflowUpdater.status);
